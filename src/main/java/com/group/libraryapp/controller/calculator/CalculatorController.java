b282ae77fafe85c3a239d446590a9e5cc103a5f0
choonghyun <a0107163@naver.com>
2024-02-20 12:00:02 +0900

    스터디 2일차

--- src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
+++ src/main/java/com/group/libraryapp/controller/calculator/CalculatorController.java
@@ -1,7 +1,10 @@
 package com.group.libraryapp.controller.calculator;
 
 import com.group.libraryapp.dto.request.CalculatorAddRequest;
+import com.group.libraryapp.dto.request.CalculatorMultiplyRequest;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +16,14 @@ public class CalculatorController {
         return number1 + number2;
     }
     */
+
     @GetMapping("/add")
     public int addTwoNumbers(CalculatorAddRequest request) {
         return request.getNumber1() + request.getNumber2();
+    }
+
+    @PostMapping("/multiply")
+    public int multiplyTwoNumbers(@RequestBody CalculatorMultiplyRequest request) {
+        return request.getNumber1() * request.getNumber2();
     }
 }
